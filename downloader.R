a30251dc7dbb1aad14395c9539a590d2ee1a8fd5
DiegoKoz <diegokoz92@gmail.com>
2019-10-31 15:04:08 -0300

    emprolijo bases 2017 y 2018

--- downloader.R
+++ downloader.R
@@ -173,3 +173,102 @@ walk2(bases$microdata,bases$filename,
       function(.x,.y){saveRDS(object = .x,file = .y)})
 
 
+### Bases post 2016
+
+## Hogar
+bases <- get_microdata(year = 2017:2018, trimester = 1:4,type = 'hogar')
+
+bases <- bases %>% 
+  mutate(filename = glue('eph/hogar/base_hogar_{year}T{trimester}.RDS'),
+         microdata = map(microdata,zap_labels),
+         microdata = map(microdata, as.tibble),
+         microdata = map(microdata, 
+                         function(x){ names(x) <- toupper(names(x))
+                         return(x)}),
+         test = file.exists(filename))
+
+# arreglo una variable que tenia diferentes tipos
+
+bases <- bases%>% 
+  mutate(microdata = map(microdata, 
+                         function(x){x %>% mutate(DECIFR = as.character(DECIFR),
+                                                  IDECIFR = as.character(IDECIFR),
+                                                  GDECIFR = as.character(GDECIFR),
+                                                  PDECIFR = as.character(PDECIFR),
+                                                  ADECIFR = as.character(ADECIFR),
+                                                  DECCFR  = as.character(DECCFR),
+                                                  IDECCFR = as.character(IDECCFR),
+                                                  RDECCFR = as.character(RDECCFR),
+                                                  GDECCFR = as.character(GDECCFR),
+                                                  PDECCFR = as.character(PDECCFR),
+                                                  ADECCFR = as.character(ADECCFR),
+                                                  RDECIFR = as.character(RDECIFR))
+                         }))
+
+test <-  bases %>% unnest(cols = c(microdata))
+
+walk2(bases$microdata,bases$filename,
+      function(.x,.y){saveRDS(object = .x,file = .y)})
+
+### individual
+
+
+bases <- get_microdata(year = 2017:2018, trimester = 1:4,type = 'individual')
+
+bases <- bases %>% 
+  filter(!(year==2015 & trimester%in%3:4),
+         !(year==2016 & trimester==1)) %>% 
+  mutate(filename = glue('eph/individual/base_individual_{year}T{trimester}.RDS'),
+         microdata = map(microdata,zap_labels),
+         microdata = map(microdata, as.tibble),
+         microdata = map(microdata, 
+                         function(x){ names(x) <- toupper(names(x))
+                         return(x)}),
+         test = file.exists(filename))
+
+# arreglo una variable que tenia diferentes tipos
+
+bases <- bases %>% 
+  mutate(microdata = map(microdata, 
+                         function(x){
+                           if (!'PP04B_COD'%in% names(x)) { x$PP04B_COD <- NA_character_}
+                           if (!'PP11B_COD'%in% names(x)) {x$PP11B_COD <- NA_character_}
+                           x %>% mutate(CH14 = as.character(CH14),
+                                        PP04B_COD = as.character(PP04B_COD),
+                                        PP04D_COD = as.character(PP04D_COD),
+                                        PP11B_COD = as.character(PP11B_COD),
+                                        PP11D_COD = as.character(PP11D_COD),
+                                        DECOCUR   = as.character(DECOCUR),
+                                        IDECOCUR  = as.character(IDECOCUR),
+                                        RDECOCUR  = as.character(RDECOCUR),
+                                        GDECOCUR  = as.character(GDECOCUR),
+                                        PDECOCUR  = as.character(PDECOCUR),
+                                        ADECOCUR  = as.character(ADECOCUR),
+                                        DECINDR   = as.character(DECINDR),
+                                        IDECINDR   = as.character(IDECINDR),
+                                        RDECINDR   = as.character(RDECINDR),
+                                        GDECINDR   = as.character(GDECINDR),
+                                        PDECINDR   = as.character(PDECINDR),
+                                        ADECINDR   = as.character(ADECINDR),
+                                        DECIFR    = as.character(DECIFR),
+                                        IDECIFR   = as.character(IDECIFR),
+                                        RDECIFR   = as.character(RDECIFR),
+                                        GDECIFR   = as.character(GDECIFR),
+                                        PDECIFR   = as.character(PDECIFR),
+                                        ADECIFR   = as.character(ADECIFR),
+                                        DECCFR    = as.character(DECCFR),
+                                        IDECCFR   = as.character(IDECCFR),
+                                        RDECCFR   = as.character(RDECCFR),
+                                        GDECCFR   = as.character(GDECCFR),
+                                        PDECCFR   = as.character(PDECCFR),
+                                        ADECCFR   = as.character(ADECCFR)
+                                        
+                           )
+                         }))
+
+test <-  bases %>% unnest(cols = c(microdata))
+
+walk2(bases$microdata,bases$filename,
+      function(.x,.y){saveRDS(object = .x,file = .y)})
+
+
