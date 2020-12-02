dd7f2b82ff28beb90dc68040c4bb33b029104727
pablotis <pablotisco@gmail.com>
2020-12-01 21:23:33 -0300

    mejoras bases 2016

--- downloader.R
+++ downloader.R
@@ -6,7 +6,7 @@ library(eph)
 
 ### 30/10/2019 Paso bases a tibble
 
-## bases puntuales hogar
+## bases puntuales hogar ----
 
 bases_hogar <- get_microdata(year = 1996:2003, wave = c(1,2),type = 'hogar')
 
@@ -20,7 +20,7 @@ bases_hogar <- bases_hogar %>%
 walk2(bases_hogar$microdata,bases_hogar$filename,
       function(.x,.y){saveRDS(object = .x,file = .y)})
 
-## bases puntuales individual
+## bases puntuales individual ----
 
 bases <- get_microdata(year = 1996:2003, wave = c(1,2),type = 'individual')
 
@@ -38,7 +38,7 @@ walk2(bases$microdata,bases$filename,
       function(.x,.y){saveRDS(object = .x,file = .y)})
 
 
-## bases continuas 2003:2007 hogar
+## bases continuas 2003:2007 hogar ----
 
 bases <- get_microdata(year = 2003:2007, trimester = 1:4,type = 'hogar')
 
@@ -56,7 +56,7 @@ walk2(bases$microdata,bases$filename,
       function(.x,.y){saveRDS(object = .x,file = .y)})
 
 
-## bases continuas 2008:2016 hogar
+## bases continuas 2008:2016 hogar ----
 
 bases <- get_microdata(year = 2008:2016, trimester = 1:4,type = 'hogar')
 
@@ -94,7 +94,7 @@ bases <- bases%>%
 walk2(bases$microdata,bases$filename,
       function(.x,.y){saveRDS(object = .x,file = .y)})
 
-## bases continuas 2003:2007 individual
+## bases continuas 2003:2007 individual ----
 
 bases <- get_microdata(year = 2003:2007, trimester = 1:4,type = 'individual')
 
@@ -244,9 +244,9 @@ walk2(bases$microdata,bases$filename,
       function(.x,.y){saveRDS(object = .x,file = .y)})
 
 
-## bases continuas 2008:2016 individual
+## bases continuas 2008:2015 individual ----
 
-bases <- get_microdata(year = 2008:2016, trimester = 1:4,type = 'individual')
+bases <- get_microdata(year = 2008:2015, trimester = 1:4,type = 'individual')
 
 bases <- bases %>% 
   filter(!(year==2015 & trimester%in%3:4),
@@ -288,9 +288,72 @@ walk2(bases$microdata,bases$filename,
       function(.x,.y){saveRDS(object = .x,file = .y)})
 
 
-### Bases post 2016
+### Bases post 2016 ----
 
-## Hogar
+# 2016 - individual
+bases <- get_microdata(year = 2016, trimester = 2:4, type = 'individual')
+
+bases <- bases %>% 
+  mutate(filename = glue('eph/individual/base_individual_{year}T{trimester}.RDS'),
+         microdata = map(microdata,zap_labels),
+         microdata = map(microdata, as.tibble),
+         microdata = map(microdata, 
+                         function(x){ names(x) <- toupper(names(x))
+                         return(x)}),
+         test = file.exists(filename))
+
+# arreglo una variable que tenia diferentes tipos
+
+bases <- bases %>% 
+  mutate(microdata = map(microdata, 
+                         function(x){
+                           if (!'PP04B_COD'%in% names(x)) {x$PP04B_COD <- NA_character_}
+                           if (!'PP11B_COD'%in% names(x)) {x$PP11B_COD <- NA_character_}
+                           
+                           x %>% mutate(CH14 = as.character(CH14),
+                                        CH15_COD = as.integer(as.character(CH15_COD)),
+                                        CH16_COD = as.integer(as.character(CH16_COD)),
+                                        PP04B_COD = as.character(PP04B_COD),
+                                        PP04D_COD = as.character(PP04D_COD),
+                                        PP11B_COD = as.character(PP11B_COD),
+                                        PP11D_COD = as.character(PP11D_COD),
+                                        IPCF      = as.numeric(as.character(IPCF)),
+                                        DECOCUR   = as.character(DECOCUR),
+                                        IDECOCUR  = as.character(IDECOCUR),
+                                        RDECOCUR  = as.character(RDECOCUR),
+                                        GDECOCUR  = as.character(GDECOCUR),
+                                        PDECOCUR  = as.character(PDECOCUR),
+                                        ADECOCUR  = as.character(ADECOCUR),
+                                        DECINDR   = as.character(DECINDR),
+                                        IDECINDR  = as.character(IDECINDR),
+                                        RDECINDR  = as.character(RDECINDR),
+                                        GDECINDR  = as.character(GDECINDR),
+                                        PDECINDR  = as.character(PDECINDR),
+                                        ADECINDR  = as.character(ADECINDR),
+                                        DECIFR    = as.character(DECIFR),
+                                        IDECIFR   = as.character(IDECIFR),
+                                        RDECIFR   = as.character(RDECIFR),
+                                        GDECIFR   = as.character(GDECIFR),
+                                        PDECIFR   = as.character(PDECIFR),
+                                        ADECIFR   = as.character(ADECIFR),
+                                        DECCFR    = as.character(DECCFR),
+                                        IDECCFR   = as.character(IDECCFR),
+                                        RDECCFR   = as.character(RDECCFR),
+                                        GDECCFR   = as.character(GDECCFR),
+                                        PDECCFR   = as.character(PDECCFR),
+                                        ADECCFR   = as.character(ADECCFR)
+                                        
+                           )
+                         }))
+
+#test <-  bases %>% unnest(cols = c(microdata))
+
+walk2(bases$microdata,bases$filename,
+      function(.x,.y){saveRDS(object = .x,file = .y)})
+
+
+
+## Hogar 2019
 bases <- get_microdata(year = 2019, trimester = 1:2,type = 'hogar')
 
 bases <- bases %>% 
@@ -326,8 +389,6 @@ walk2(bases$microdata,bases$filename,
       function(.x,.y){saveRDS(object = .x,file = .y)})
 
 ### individual
-
-
 bases <- get_microdata(year = 2019, trimester = 1:2,type = 'individual')
 
 bases <- bases %>% 
